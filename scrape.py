4e7a3a509587273dd2fa685750bf13e35ba4a25c
fellakmoh876 <mohammedfellak560@gmail.com>
2019-10-18 07:15:56 -0400

    regular expressions

--- scrape.py
+++ scrape.py
@@ -13,3 +13,20 @@ main_content = soup.find('div', attrs = {'class': 'entry-content'})
 
 # Extract the relevant information as text
 content = main_content.find('ul').text
+
+# Create a pattern to match names
+name_pattern = re.compile(r'^([A-Z]{1}.+?)(?:,)', flags = re.M)
+# Find all occurrences of the pattern
+names = name_pattern.findall(content)
+
+# Make school pattern and extract schools
+school_pattern = re.compile(r'(?:,|,\s)([A-Z]{1}.*?)(?:\s\(|:|,))')
+schools = school_pattern.findall(content)
+# Pattern to match the salaries
+salary_pattern = re.compile(r'\$.+')
+salaries = salary_pattern.findall(content)
+
+# Messy salaries
+salaries = ['$876,001', '$543,903', '$2453,896']
+# Convert salaries to numbers in a list comprehension 
+[int(''.join(s[1:].split(','))) for s in salaries]
